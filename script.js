8771917205ffa08989db17c56fff53ff30327659
mttt <groobytt@gmail.com>
2022-11-05 12:01:07 +0100

    finish adding feature

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ const submitBtns = document.querySelectorAll('.submit-form')
 
 submitBtns.forEach(button=>
 button.addEventListener('click',(e)=>{
-    console.log(e.target)
+   // console.log(e.target)
     let mode =null
     if(e.target.id == "submit-btn-add") mode = 'add'
     else if(e.target.id == "submit-btn-edit") mode = 'edit'
@@ -44,16 +44,70 @@ button.addEventListener('click',(e)=>{
     if(mode=='add'){
         let book = new Book(titleF,authorF,isReadF,pagesF,imageUrlF,colorF)
         myLibrary.push(book)
-        console.log(myLibrary)
+       // console.log(myLibrary)
     }
 
 
-
-    updateLibraryView()
+    showLibrary()
 }))
 
 
-function updateLibraryView(){
+
+
+
+function showLibrary(){ 
+    clearLibrary()
+    const card = document.querySelector(".bookBoilerPrint")
+    for(let i =0;i<myLibrary.length;i++){
+        const newCard = card.cloneNode(true)
+        newCard.style.display="flex"
+        newCard.dataset.index=`${i}`
+        newCard.classList.remove("bookBoilerPrint")
+        newCard.classList.add("book")
+
+       
+
+
+        bookshelf.appendChild(newCard)
+
+    //NW - new book
+    let NB=document.querySelector(`[data-index="${i}"]`)
+    let NBchildren = NB.childNodes
+    
+
+    //title
+    if(myLibrary[i].title=="") NBchildren[1].innerText = "no title"
+    else NBchildren[1].innerText = myLibrary[i].title
+
+    //cover
+    if(myLibrary[i].cover=="") NBchildren[3].firstChild.setAttribute("src","/images/nocover.png")
+    else NBchildren[3].firstChild.setAttribute("src",`${myLibrary[i].cover}`)
+
+    //author
+    if(myLibrary[i].author=="") NBchildren[5].innerText = "unknown author"
+    else NBchildren[5].innerText = myLibrary[i].author
+
+    //checkbox
+    if(myLibrary[i].isRead) NBchildren[7].getElementsByTagName('input')[0].setAttribute("checked","")
+    else  NBchildren[7].getElementsByTagName('input')[0].removeAttribute("checked")
+    
+    //pages
+    if(myLibrary[i].pages=="") NBchildren[9].innerText="pages: ???"
+    NBchildren[9].innerText = "pages:"+myLibrary[i].pages
+
+    //color
+    newCard.style.borderLeft = `solid ${myLibrary[i].color} 4px`
+   
+}
+}
+
+function clearLibrary(){
+    let toDelete = document.querySelectorAll(".book")
+    //console.log(toDelete)
+     toDelete.forEach(element=>{
+        //console.log("DELETED", element)
+        bookshelf.removeChild(element)
+     })
     
 }
 
